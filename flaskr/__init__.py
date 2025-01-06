b4620d6d668bd0592449effc2e6c0c9f9c50d02f
Avinash <avinash.kc3@gmail.com>
2025-01-06 16:32:16 +0530

    Added database setup and initialization

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -22,6 +21,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    from . import db  # Import db here
+    db.init_app(app)  # Register db functions
 
     @app.route('/hello')
     def hello():
